260aec69bdeaa23a31cf6b9fa4061b631b18d089
Mohammed Abdulwahhab <furkhan324@gmail.com>
2016-09-28 17:16:18 -0700

    Update person.rb

--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -1,20 +1,20 @@
-+class Person
- +  def initialize(name,  age)
- +    @pname = name
- +    @page = age
- +    @pnick = name[0..3]
- +  end
- +
- +  def introduce
- +    "#{@pname} #{@page}"
- +  end
- +
- +  def age
- +    2016 - @page.to_i
- +  end
- +
- +  def nickname
- +    "#{@pnick}"
- +  end
- +
- +end
+class Person
+   def initialize(name,  age)
+     @pname = name
+     @page = age
+     @pnick = name[0..3]
+   end
+ 
+   def introduce
+     "#{@pname} #{@page}"
+   end
+ 
+   def age
+     2016 - @page.to_i
+   end
+ 
+   def nickname
+     "#{@pnick}"
+   end
+ 
+ end
