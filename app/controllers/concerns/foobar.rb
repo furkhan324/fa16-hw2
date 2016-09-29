198458347cc0afe520a55f844f88256a9587ebb7
Mohammed Abdulwahhab <furkhan324@gmail.com>
2016-09-28 17:14:35 -0700

    Update foobar.rb

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,11 @@
-class Foobar
-  # ENTER CODE FOR Q2 HERE
-  def initialize(param)
-  end
-end
++class Foobar
+ +  # ENTER CODE FOR Q2 HERE
+ +  def initialize(str)
+ +    @myStr = str
+ +  end
+ +
+ +  def bar(arg1, arg2={})
+ +    "#{arg1}#{@myStr}#{arg2[:sat]}"
+ +
+ +  end
+ +end
